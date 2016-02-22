60a1636376f1f363712512815513d3cac6ff862f
keezbox <dolbyhome@abv.bg>
2016-02-22 11:23:17 +0200

    Update vanko.js

--- vanko.js
+++ vanko.js
@@ -1,7 +1,7 @@
 // JavaScript Document
- if (screen.width < 1364) { 
+ if (screen.width < 136) { 
     window.location="http://mobile.juicyads.com/service_redirect.php?p=79987&s=155361&u=http%3A%2F%2Fliquidfire.juicyads.mobi%2F%3Fcampaign%3D2747%26track%3D155361";
  }  
-  if (screen.width > 1365) { 
+  if (screen.width > 136) { 
     window.location="http://xapi.juicyads.com/service_advanced.php?juicy_code=94e43323w256r2t2t2943364&u=http%3A%2F%2Fwww.bustyg.com";
  } 
